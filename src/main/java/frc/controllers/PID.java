13f68bdffc4a6c3814a056497a87865bc5fc9c73
Harsh Padhye <harshpadhye@gmail.com>
2019-01-20 15:54:00 -0500

    Update PID.java

--- src/main/java/frc/controllers/PID.java
+++ src/main/java/frc/controllers/PID.java
@@ -7,6 +7,11 @@
 
 package frc.controllers;
 
+import edu.wpi.first.wpilibj.PIDOutput;
+import edu.wpi.first.wpilibj.PIDSource;
+import edu.wpi.first.wpilibj.PIDSourceType;
+import edu.wpi.first.wpilibj.Timer;
+
 /**
  * Add your docs here.
  */
@@ -15,25 +20,59 @@ public class PID implements Controller {
     double kP, kI, kD; //control gains (proportional, integral, derivative)
     double setpoint;
     double errorSum;
+    double lastError;
+    double lastTime;
+    double tolerance;
+
+    int cycles;
+
     boolean done;
 
     public PID(double kP, double kI, double kD) {
         this.kP = kP;
         this.kI = kI;
         this.kD = kD;
-
         errorSum = 0.0;
+        lastError = 0.0;
+        lastTime = 0.0;
+        tolerance = 0.0;
+
+        cycles = 0;
 
         done = true;
     }
 
     public void setSetpoint(double setpoint) {
         done = false;
+        errorSum = 0.0;
         this.setpoint = setpoint;
     }
 
     public double calculate(double current) {
-        return 0.0;
+        double error = setpoint - current; //difference between desired value and current value
+        double time = Timer.getFPGATimestamp() * 1000; //current time
+        double dt = time - lastTime; //timestep
+        double errorRate = (error - lastError) / dt; //rate of change of error with respect to time
+
+        errorSum += error * dt; //integrates error over time
+
+        // sets the last values for the next iteration
+        lastTime = time;
+        lastError = error;
+
+        if(Math.abs(error) < tolerance) {
+            cycles++;
+        } else {
+            cycles = 0;
+        }
+
+        done = (cycles > 5); //checks 5 times whether the robot is on target
+
+        if(done) {
+            return 0.0;
+        }
+
+        return (kP * error) + (kI * errorSum) + (kD * errorRate);
     }
 
     public boolean isDone() {
